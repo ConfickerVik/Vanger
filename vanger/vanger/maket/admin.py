508a2456ff8c730fb776c559615f5abf898301a6
ConfickerVik <viktor.kf.98@mail.ru>
2024-05-14 21:23:17 +0800

    added pictures in admin panel

--- vanger/vanger/maket/admin.py
+++ vanger/vanger/maket/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import SliderImage
 from adminsortable2.admin import SortableAdminMixin
+from django.utils.safestring import mark_safe
+
+from .models import SliderImage
 
 
 @admin.register(SliderImage)
 class SliderImageAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ["my_order", "name", "cover"]
+    readonly_fields = ['image_thumb']
+    list_display = ["my_order", "image_thumb",  "name", "cover"]
+
+    def image_thumb(self, obj):
+        if obj.cover:
+            return mark_safe('<img src="%s" width="80px" height="80px" style="object-fit: contain;" />' % obj.cover.url)
+        else:
+            return 'None'
+
+    image_thumb.short_description = 'Pictures'
